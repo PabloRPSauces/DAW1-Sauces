dfb8c757d0a7f49fb79c304c2b6cf5ee3fbbfaef
Pablo <Pablo@DESKTOP-HF5M7M3>
2020-05-18 09:09:35 +0200

    Añadido Calculadora

---
+++ Tema 5 - Pruebas/JUnit/Cafetera/Cafetera.java
@@ -0,0 +1,44 @@
+package pruebas;
+
+public class Cafetera {
+	private int capacidadMaxima; 
+	private int cantidadActual;
+
+	public Cafetera() { 
+
+	}
+
+	public Cafetera(int capacidad) { 
+
+	}
+
+	public Cafetera(int capacidad, int cantidad) { 
+
+	}
+
+
+	public void setCapacidadMaxima(int c) { 
+	
+	}
+	public void setCantidadActual(int c) { 
+
+	}
+
+	public void llenarCafetera() { 
+
+	}
+
+	public int servirTaza(int cc) { 
+		
+	}
+
+	public void vaciarCafetera() { 
+	
+	}
+
+	public void agregarCafe(int cc) { 
+	
+	}
+
+
+}
